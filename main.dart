41234af5502a98502a36573fbaf5b1c346954f7a
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-19 09:55:14 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+void main(){
+  
+
+
+  // print("Enter your name: ");
+  // String? name = stdin.readLineSync();
+
+  // print("Your name is: $name");
+
+  // print('Enter your father name: ');
+  // String? fatherName = stdin.readLineSync();
+  // print("Your father name is: $fatherName");
+
+  // print('Enter your age: ');
+  // int age = int.parse(stdin.readLineSync()!);
+  // print("Your age is: $age");
+
+  // print('Enter your Email Address');
+
+  // String? Email = stdin.readLineSync();
+  // print("Your Email Address is: $Email");
+
+
+  // stdout.writeln('I am a software engineer: ');
+
+
+  // print('Write any type of data: ');
+
+  // var data = stdin.readLineSync();
+  // print(data);  
+  
+
+
+  // stdout.writeln('Enter enter num1: ');
+  // int num1 = int.parse(stdin.readLineSync()!);
+
+  // stdout.writeln('Enter enter num2: ');
+  // int num2 = int.parse(stdin.readLineSync()!);
+
+  // int sum = num1 + num2;
+  // print('The sum of : $sum');
+
+
+  // stdout.writeln('Enter the value in feet: ');
+  // double num1 = double.parse(stdin.readLineSync()!);
+
+  // double result = num1 / 3.2;
+
+  // print(result);
+
+
+  stdout.writeln('Enter temperature in c: ');
+  double c = double.parse(stdin.readLineSync()!);
+
+  double f = (c * 9/5) + 32;
+  print('Temperature in fahrenheit is: $f');
+
+
+
+
+
+
+
+
+
+
+
+}
