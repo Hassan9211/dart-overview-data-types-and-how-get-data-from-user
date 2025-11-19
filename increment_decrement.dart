1f05ab06ecd017ec8b503bb8d726a530737bea3f
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-19 15:29:27 +0500

    Add files via upload

---
+++ increment_decrement.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) {
+  //---Postfex increment decrement ---
+  
+  int a = 10;
+
+  a++;
+
+  print(a);
+
+
+  int b = 20;
+
+  b--;
+
+  print(b);
+
+
+
+  //---Prefix increment decrement ---
+
+  int x = 30;
+
+  
+
+  print(++x);
+
+  int y = 40;
+
+
+  print(--y);
+
+
+
+}
