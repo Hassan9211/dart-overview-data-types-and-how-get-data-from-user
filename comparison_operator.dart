fa26a0c27f6e8870b261e027856a7de76e3130c5
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-19 10:34:12 +0500

    Add files via upload

---
+++ comparison_operator.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+
+  int a = 10;
+  int b = 10;
+
+   bool greater , equal , smaller , notEqual , lessEqual , greaterEqual;
+
+  //--- comparison operators ---
+  // > , < , == , != , <= , >=
+
+  greater = a > b;
+  equal = a == b;
+  smaller = a < b;
+  notEqual = a != b;
+  lessEqual = a <= b;
+  greaterEqual = a >= b;
+
+
+
+  print('a > b : $greater');
+  print('a == b : $equal');
+  print('a < b : $smaller');
+  print('a != b : $notEqual');
+  print('a <= b : $lessEqual');
+  print('a >= b : $greaterEqual');
+
+  
+  
+
+  
+
+}
