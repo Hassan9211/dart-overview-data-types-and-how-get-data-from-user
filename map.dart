c690703531d1e0f4f71cc7d9b4b3644ce0a20d73
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-20 16:19:46 +0500

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  
+  // Map<String, dynamic> studentMarks = {
+  //   'Hassan Raza': 85,
+  //   'Ali Khan': 90,
+  //   'Ayesha': 95,
+  //   'Zainab': 88,
+  // };
+
+  
+  // print(studentMarks);
+
+  Map <dynamic, dynamic> postalCodes = {
+    63000 : 'hasilpur',
+    63100 : 'bahawalpur',
+    64000 : 'vehari',
+    62000 : 'bahawalnagar',
+    45000 : 'multan',
+  };
+  print(postalCodes);
+
+
+  Map <String, String> bloodgroups = {
+    'Hassan' : 'B+',
+    'Mohsan' : 'A+',
+    'Ali' : 'A+',
+    'Abdul Rehman' : 'A+',
+    'Aqeel' : 'B+',
+  };
+  print(bloodgroups);
+
+
+}
