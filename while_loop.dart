b6521022b45779d66b41ad6d59b7cf8c1e88be29
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-20 11:11:01 +0500

    Add files via upload

---
+++ while_loop.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  
+
+  // int a = 1;
+  // while (a <= 10) {
+  //   print('Hello World $a');
+  //   a++;
+  // }
+
+  // while (true) {
+  //   print('Hassan Raza');
+  // }
+
+
+  int b = 1;
+  while (b <= 10) {
+    print('5 x $b = ${5 * b}');
+    b++;
+  }
+
+
+}
